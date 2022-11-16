37897644e9a0ff93db01009c1daaf2dd35df2261
Jose <josecarvajalhilario@gmail.com>
2022-11-16 13:11:26 +0100

    Improve quarkus-openshift example

--- openshift/src/main/java/acme/GreetingResource.java
+++ openshift/src/main/java/acme/GreetingResource.java
@@ -3,11 +3,16 @@ package acme;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 @Path("/greetings")
 public class GreetingResource {
 
+    @ConfigProperty(name = "hello.message")
+    String message;
+
     @GET
     public String hello() {
-        return "Hello, world!";
+        return message;
     }
 }
