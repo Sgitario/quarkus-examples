44f84644e5e9c191d65c9c121e8b8d09c12a495b
Jose <josecarvajalhilario@gmail.com>
2022-12-07 11:23:45 +0100

    Update openshift example with arguments

--- openshift/src/main/java/acme/GreetingResource.java
+++ openshift/src/main/java/acme/GreetingResource.java
@@ -1,7 +1,11 @@
 package acme;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
@@ -14,5 +18,13 @@ public class GreetingResource {
     @GET
     public String hello() {
         return message;
+    }
+
+    @GET
+    @Path("/arguments")
+    public String argument() {
+        return ProcessHandle.current().info().arguments().stream()
+                .flatMap(l -> Arrays.stream(l))
+                .collect(Collectors.joining( " "));
     }
 }
