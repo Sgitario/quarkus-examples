75f6556146b16a24cad9129ed4911ada1c2a72fd
Jose <josecarvajalhilario@gmail.com>
2023-01-18 08:44:53 +0100

    Extend knative example with microprofile

--- kubernetes-knative/src/main/java/acme/GreetingResource.java
+++ kubernetes-knative/src/main/java/acme/GreetingResource.java
@@ -1,13 +1,20 @@
 package acme;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+
+import io.micrometer.core.instrument.MeterRegistry;
 
 @Path("/greetings")
 public class GreetingResource {
 
+    @Inject
+    MeterRegistry registry;
+
     @GET
     public String hello() {
+        registry.counter("greeting_counter").increment();
         return "Hello, world!";
     }
 }
